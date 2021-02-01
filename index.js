c84c413798401d1253772c2e7245e29df8d67d04
Claudio Garcia Iglesia <claudio@fsdev.wonderbyte>
2021-02-01 04:01:38 -0300

    fix listen port to work with heroku

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.post('/form', (req, res) => {
     res.send(htmlResponse);
 })
 
-app.listen(3000, function () {
-    console.log("Example app listening on port 3000!");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
